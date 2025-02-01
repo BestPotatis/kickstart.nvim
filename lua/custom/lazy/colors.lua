b5bd84ed144db5b3f5b06bf378b1c64f9bc3eb7a
Oliver s194606 <s194606@student.dtu.com>
2025-02-01 17:55:18 +0100

    Change colorscheme to everforest

--- lua/custom/lazy/colors.lua
+++ lua/custom/lazy/colors.lua
@@ -4,14 +4,15 @@ return {
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'rebelot/kanagawa.nvim',
+  'sainnhe/everforest',
 
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'kanagawa'
+    vim.g.everforest_diagnostic_virtual_text = 1
+    vim.cmd.colorscheme 'everforest'
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
   end,
